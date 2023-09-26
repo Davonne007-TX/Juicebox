5e08f25d3f77e88bf1f6ef9759d90930f8d50300
Vonney <davonnejv@gmail.com>
2023-09-26 18:10:26 -0500

    adding Requirements of Project

---
+++ db/Requirements.js
@@ -0,0 +1,16 @@
+/*
+
+Requirements
+- A project plan using GitHub Projects
+- Functioning RESTful API that satisfies project requirements, consisting of:
+  - A repaired PostgreSQL database schema.
+  - A repaired Express.js endpoint.
+  - A working JSON Web Token, bcrypt authorization, and authentication middleware.
+  - One newly created API endpoint. Suggest endpoint to serve frontend application.
+  - The use of one junction table
+  - PostgreSQL with SQL queried tables, rows, and columns
+  - At least one foreign key constraint
+  - Established RESTful endpoints using Express.js
+  - A GET endpoint to render static HTML or React.js single page application.
+
+*/
